53a9e4e358dedf4f87ef2268397fd80418a25373
Sanket Ghanmare <sanketghanmare@gmail.com>
2020-08-12 21:59:16 -0400

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,7 @@ import './header.component.scss';
 import {auth} from "../../firebase/firebase.utils";
 import {connect} from 'react-redux'
 import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-deopdown.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
